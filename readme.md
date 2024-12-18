38dc20491e4771193f2bc9546c909586e1d0e712
MouseScroller <8330251+MouseScroller@users.noreply.github.com>
2024-12-18 01:07:53 +0100

    -

--- readme.md
+++ readme.md
@@ -12,6 +12,10 @@ tries to run the executable
 ### release
 tries to make a develop release
 
+### lint
+run a linter over file/project
+
+
 ## Supported
 - Makefile
 - Cargo project
@@ -24,5 +28,30 @@ tries to make a develop release
 .rs
 .cpp
 .c
+.lua
 .bash
 .sh
+
+### Dependencyes
+
+## Lua
+- lua
+- luacheck
+
+## JavaScript
+- node
+- eslint
+
+## Bash
+- bash
+- shellcheck
+
+## Rust
+- rustc
+- cargo
+
+## C++
+- g++
+
+## C
+- gcc
--- src/main.rs
+++ src/main.rs
@@ -11,94 +11,92 @@ enum Target {
 	C(String),
 	Rust(String),
 	Js(String),
+	Lua(String),
 	Bash(String),
 }
 
 impl Target {
 	fn get_filename(&self) -> Option<String> {
 		match self {
-			Target::Bash(x) | Target::Js(x) | Target::Cpp(x) | Target::Rust(x) | Target::C(x) => {
-				Some(x.to_string())
-			}
+			Target::Bash(x)
+			| Target::Js(x)
+			| Target::Cpp(x)
+			| Target::Rust(x)
+			| Target::C(x)
+			| Target::Lua(x) => Some(x.to_string()),
 			Target::Make => Some("Makefile".to_owned()),
 			Target::Cargo => Some("Cargo.toml".to_owned()),
 		}
 	}
 	fn get_binary(&self) -> Option<String> {
 		match self {
-			Target::Bash(x) | Target::Js(x) => return Some(x.to_string()),
+			Target::Bash(x) | Target::Js(x) | Target::Lua(x) => Some(x.to_string()),
 			Target::Cpp(x) | Target::Rust(x) | Target::C(x) => {
 				let mut bin = x.clone();
 				bin.truncate(bin.find(".").unwrap());
-				return Some(bin);
+				Some(bin)
 			}
 			Target::Make => {
 				let file = File::open("Makefile").unwrap();
 				let lines = BufReader::new(file).lines();
 				let target = Regex::new("^TARGET\\s*:=\\s*(\\w+)").expect("Regex error");
 
-				for line in lines {
-					if let Ok(line) = line {
-						let mat = target.captures(&line);
-						if let Some(mat) = mat {
-							let val = mat.get(1);
-							if let Some(val) = val {
-								return Some(val.as_str().to_owned());
-							}
+				for line in lines.into_iter().flatten() {
+					let mat = target.captures(&line);
+					if let Some(mat) = mat {
+						let val = mat.get(1);
+						if let Some(val) = val {
+							return Some(val.as_str().to_owned());
 						}
 					}
 				}
-				return None;
+				None
 			}
 			Target::Cargo => {
 				let file = File::open("Cargo.toml").unwrap();
 				let lines = BufReader::new(file).lines();
 				let target = Regex::new("^name\\s*=\\s*\"(\\w+)\"").expect("Regex error");
 
-				for line in lines {
-					if let Ok(line) = line {
-						let mat = target.captures(&line);
-						if let Some(mat) = mat {
-							let val = mat.get(1);
-							if let Some(val) = val {
-								return Some(val.as_str().to_owned());
-							}
+				for line in lines.into_iter().flatten() {
+					let mat = target.captures(&line);
+					if let Some(mat) = mat {
+						let val = mat.get(1);
+						if let Some(val) = val {
+							return Some(val.as_str().to_owned());
 						}
 					}
 				}
-				return None;
+				None
 			}
 		}
 	}
 
 	fn handle_build_result(&self, return_code: i32, _stdout: Option<u8>) -> bool {
-		match self {
-			_ => {
-				if return_code != 0 {
-					return false;
-				}
-				return true;
-			}
+		if return_code != 0 {
+			return false;
 		}
+		true
 	}
 }
 
 fn update_target(old_target: Option<Target>, new_target: Option<Target>) -> Option<Target> {
 	match (old_target, new_target) {
-		(Some(Target::Make), _) => return Some(Target::Make),
-		(_, Some(Target::Make)) => return Some(Target::Make),
-		(Some(Target::Cargo), _) => return Some(Target::Cargo),
-		(_, Some(Target::Cargo)) => return Some(Target::Cargo),
-		(_, Some(x)) => return Some(x),
-		(_, _) => return None,
+		(Some(Target::Make), _) => Some(Target::Make),
+		(_, Some(Target::Make)) => Some(Target::Make),
+		(Some(Target::Cargo), _) => Some(Target::Cargo),
+		(_, Some(Target::Cargo)) => Some(Target::Cargo),
+		(_, Some(x)) => Some(x),
+		(_, _) => None,
 	}
 }
 
 fn endings(file_name: &str) -> Option<Target> {
 	if file_name.ends_with(".js") {
 		return Some(Target::Js(file_name.to_string()));
-	} else if file_name.ends_with(".cpp") {
+	} else if file_name.ends_with(".cpp") || file_name.ends_with(".cxx") {
 		return Some(Target::Cpp(file_name.to_string()));
+	} else if file_name.ends_with(".lua") {
+		return Some(Target::Lua(file_name.to_string()));
 	} else if file_name.ends_with(".bash") || file_name.ends_with(".sh") {
 		return Some(Target::Bash(file_name.to_string()));
 	} else if file_name.ends_with(".rs") {
@@ -115,12 +113,14 @@ fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
 	let mut run = false;
 	let mut build = false;
 	let mut release = false;
+	let mut lint = false;
 
 	for arg in std::env::args() {
 		match arg.as_str() {
 			"build" => build = true,
 			"run" => run = true,
 			"release" => release = true,
+			"lint" => lint = true,
 			_ => continue,
 		}
 	}
@@ -136,20 +136,96 @@ fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
 				}
 				"Cargo.toml" => target = update_target(target, Some(Target::Cargo)),
 				_ => {
-					if target == None {
-						if entry.starts_with("main.") {
-							target = update_target(target,endings(entry));
-						} else if entry.starts_with("index.") {
-							target = update_target(target, endings(entry));
-						} else if entry.starts_with("test.") {
-							target = update_target(target, endings(entry));
-						}
+					if target.is_none()
+						&& (entry.starts_with("main.")
+							|| entry.starts_with("index.")
+							|| entry.starts_with("test."))
+					{
+						target = update_target(target, endings(entry));
 					}
 				}
 			}
 		}
 	}
 
+	if lint {
+		if let Some(ref target) = target {
+			println!("==== Build target ({})", target.get_filename().unwrap());
+
+			let mut command = match target {
+				Target::Make => {
+					let mut command = Command::new("make");
+					if release {
+						command.arg("lint");
+					}
+					command
+				}
+				Target::Cargo => {
+					let mut command = Command::new("cargo");
+					command.arg("fmt");
+					command
+				}
+
+				Target::Cpp(ref file) => {
+					let mut command = Command::new("g++");
+					command.arg(file);
+					command.arg("-o");
+					command.arg(target.get_binary().unwrap());
+					if release {
+						command.arg("-O3");
+					}
+					command
+				}
+				Target::C(ref file) => {
+					let mut command = Command::new("gcc");
+					command.arg(file);
+					command.arg("-o");
+					command.arg(target.get_binary().unwrap());
+					if release {
+						command.arg("-O3");
+					}
+					command
+				}
+				Target::Rust(ref file) => {
+					let mut command = Command::new("rustc");
+					command.arg(file);
+					command
+				}
+				Target::Js(ref file) => {
+					let mut command = Command::new("eslint");
+					command.arg("--env").arg("es6").arg(file);
+					command
+				}
+				Target::Lua(ref file) => {
+					let mut command = Command::new("luacheck");
+					command.arg("-q").arg(file);
+					command
+				}
+				Target::Bash(ref file) => {
+					let mut command = Command::new("shellcheck");
+					command.arg("--norc").arg("--severity=style").arg(file);
+					command
+				}
+			};
+
+			let child = command.spawn();
+			if let Ok(mut child) = child {
+				let ret = child
+					.wait()
+					.map_or(127, |code| code.code().expect("==== Linting terminated"));
+
+				if target.handle_build_result(ret, None) {
+					println!("==== Linting Done");
+				} else {
+					println!("==== Linting Failed [{}]", ret);
+				}
+			} else {
+				println!("==== Failed to run lint command")
+			}
+		} else {
+			println!("==== No lint target found");
+		}
+	}
 	if build || release {
 		if let Some(ref target) = target {
 			println!("==== Build target ({})", target.get_filename().unwrap());
@@ -201,15 +277,20 @@ fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
 					command.arg("--env").arg("es6").arg(file);
 					command
 				}
+				Target::Lua(ref file) => {
+					let mut command = Command::new("luacheck");
+					command.arg("-q").arg(file);
+					command
+				}
 				Target::Bash(ref file) => {
 					let mut command = Command::new("shellcheck");
-					command.arg(file);
+					command.arg("--norc").arg("--severity=warning").arg(file);
 					command
 				}
 			};
 
-			let child = command.spawn().map_or(None, |c| Some(c));
-			if let Some(mut child) = child {
+			let child = command.spawn();
+			if let Ok(mut child) = child {
 				let ret = child
 					.wait()
 					.map_or(127, |code| code.code().expect("==== Build terminated"));
@@ -218,7 +299,7 @@ fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
 					println!("==== Build Successfull");
 				} else {
 					run = false;
-					println!("==== Build Failed");
+					println!("==== Build Failed [{}]", ret);
 				}
 			} else {
 				println!("==== Failed to run build command")
@@ -230,11 +311,10 @@ fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
 	}
 
 	if run {
-		if let Some(ref target) = target  {
-
+		if let Some(ref target) = target {
 			let binary = target.get_binary();
-			if binary == None {
-				println!("==== No target to run found {:?}",target);
+			if binary.is_none() {
+				println!("==== No target to run found {:?}", target);
 				process::exit(2);
 			}
 			let binary = binary.unwrap();
@@ -242,8 +322,7 @@ fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
 
 			let mut command = match target {
 				Target::Make | Target::C(_) | Target::Cpp(_) | Target::Rust(_) => {
-					let command = Command::new(format!("./{}", binary));
-					command
+					Command::new(format!("./{}", binary))
 				}
 				Target::Cargo => {
 					let mut command = Command::new("cargo");
@@ -255,23 +334,28 @@ fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
 				}
 				Target::Js(_) => {
 					let mut command = Command::new("node");
-					command.arg(format!("./{}",binary));
+					command.arg(format!("./{}", binary));
+					command
+				}
+				Target::Lua(_) => {
+					let mut command = Command::new("lua");
+					command.arg(format!("./{}", binary));
 					command
 				}
 				Target::Bash(_) => {
 					let mut command = Command::new("bash");
-					command.arg(format!("./{}",binary));
+					command.arg(format!("./{}", binary));
 					command
 				}
 			};
 
-			let child = command.spawn().map_or(None, |c| Some(c));
-			if let Some(mut child) = child {
+			let child = command.spawn();
+			if let Ok(mut child) = child {
 				let ret = child
 					.wait()
 					.map_or(127, |code| code.code().expect("==== Build terminated"));
 
-				println!("==== Run return code [{}]",ret);
+				println!("==== Run return code [{}]", ret);
 			} else {
 				println!("==== Failed to run programm");
 			}
